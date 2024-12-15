fd51958f905f171c9df3e30bbeec1b50a2e02e81
Mr-bob-kou <bob323565@gmail.com>
2024-12-15 22:50:07 +0800

    Update 6_📦_Adding_Heritage.py

--- pages/6_📦_Adding_Heritage.py
+++ pages/6_📦_Adding_Heritage.py
@@ -24,7 +24,7 @@ with tab1:
             type=st.selectbox("Type",tp) 
             danger = st.radio("Is this Heritage in Danger?", ["Yes", "No"])
         with co2:
-            m.add_marker([x_cord,y_cord])
+            t=m.marker_cluster(event='click', add_marker=True)
             m.to_streamlit(width=500, height=500)
         submitted = st.form_submit_button("Submit")
     if submitted:
@@ -32,3 +32,4 @@ with tab1:
         st.write("Year",year)
         st.write("description",description)
         st.write(x_cord)
+        st.write(t)
