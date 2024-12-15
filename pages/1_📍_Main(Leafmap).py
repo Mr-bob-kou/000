deaf3f4b978cb500431a126d766d71d65e73a9ad
Mr-bob-kou <bob323565@gmail.com>
2024-12-15 23:30:57 +0800

    Update 1_📍_Main(Leafmap).py

--- pages/1_📍_Main(Leafmap).py
+++ pages/1_📍_Main(Leafmap).py
@@ -122,10 +122,15 @@ with col1:
     m = leafmap.Map(center=[40, -100], zoom=4)
     if mode=='Choropleth Map(Heritage Count)':
         chromap(data2,m)
-        st.write("#### Heritage Count Statistics(Top 10)")
-        col3,col4,col5=st.columns([2,1,1])
+        
+        col3,col4=st.columns([2,2])
         with col3:
+            st.write("#### Heritage Count Statistics(Top 10)")
             charts = alt.Chart(count10).mark_bar(size=20).encode(x=alt.X("name",type="nominal").sort("y"),y=alt.Y("count",type="quantitative"))
+            st.altair_chart(charts,use_container_width=True)
+        with col4:
+            st.write("#### Heritage Count Pie Chart")
+            alt.Chart(Count).mark_arc().encode(theta="count",color="name")
             st.altair_chart(charts,use_container_width=True)
             
     elif mode=='Heat Map':
