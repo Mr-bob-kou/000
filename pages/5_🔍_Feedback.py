2c4ce52457a1e7bf2a5c699060193c230979535b
Mr-bob-kou <bob323565@gmail.com>
2024-12-16 22:17:38 +0800

    Update 5_🔍_Feedback.py

--- pages/5_🔍_Feedback.py
+++ pages/5_🔍_Feedback.py
@@ -10,7 +10,7 @@ selected = st.feedback("stars")
 but=st.button("Send Feedback")
 if but:
     @st.dialog("Thank You~", width="small")
-    st.write("Thanks~~~")
+      st.write("Thanks~~~")
 if selected is not None:
     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
 
