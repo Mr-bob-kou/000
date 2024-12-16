496e95353a6f1e5eab4a88dd1de4cf264be9ce69
Mr-bob-kou <bob323565@gmail.com>
2024-12-16 20:46:46 +0800

    Update 4_📦_Adding_Heritage.py

--- pages/4_📦_Adding_Heritage.py
+++ pages/4_📦_Adding_Heritage.py
@@ -11,6 +11,7 @@ st.title("Adding!!")
 yr_range=list(range(1900,2100))
 tp=["Natural","Cultural","Mixed"]
 tab1, tab2=st.tabs(["Add Heritage", "Delete Heritage"])
+st.session_state
 with tab1:
     with st.form("my_form"):
         st.write("Inside the form")
