c925dcd8664328cffc97b10a3418ea33a92fb5af
Mr-bob-kou <bob323565@gmail.com>
2024-12-16 21:46:23 +0800

    Update 4_📦_Adding_Heritage.py

--- pages/4_📦_Adding_Heritage.py
+++ pages/4_📦_Adding_Heritage.py
@@ -37,12 +37,12 @@ with tab1:
             m.to_streamlit(width=500, height=500)
         submitted = st.form_submit_button("Submit")
     if submitted:
-        if st.session_state.danger=="Yes":
-            st.write("Name", name, "Country", country)
-            st.write("Year",year)
-            st.write("description",description)
-            st.write(x_cord)
-        elif st.session_state.danger=="No":
-            st.write("hello")
+        df1={'NAME':name,
+            'COUNTRY':country,
+            'INSCRIBDATE':year,
+            'Description':description,
+            'LONGITUDE':x_cord,
+            'LATITUDE':y_cord}
+        st.dataframe(data=df1,use_container_width=True)
 with tab2:
     st.dataframe(data=st.session_state.heritage1, use_container_width=True)
