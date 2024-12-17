bc6ac5ce02b2f10402dd3cfe35dd78d34bd35afa
Mr-bob-kou <bob323565@gmail.com>
2024-12-17 16:46:15 +0800

    Update 4_📦_Adding_Heritage.py

--- pages/4_📦_Adding_Heritage.py
+++ pages/4_📦_Adding_Heritage.py
@@ -52,7 +52,7 @@ with tab1:
             'LONGITUDE':float(x_cord),
             'LATITUDE':float(y_cord),
             "TRANSBOUND ":num}
-        #gdf = gpd.GeoDataFrame(df1, geometry=gpd.points_from_xy(df1['LONGITUDE'], df1['LATITUDE']))
-        st.dataframe(data=df1,use_container_width=True)
+        gdf = gpd.GeoDataFrame(df1, geometry=gpd.points_from_xy(df1['LONGITUDE'], df1['LATITUDE']))
+        st.dataframe(data=gdf,use_container_width=True)
 with tab2:
     st.dataframe(data=st.session_state.heritage1, use_container_width=True)
