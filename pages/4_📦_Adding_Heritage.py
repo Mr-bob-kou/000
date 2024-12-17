b32fd347168f0a64d5ff40e19e5057cbfd5cb774
Mr-bob-kou <bob323565@gmail.com>
2024-12-17 17:18:39 +0800

    Update 4_📦_Adding_Heritage.py

--- pages/4_📦_Adding_Heritage.py
+++ pages/4_📦_Adding_Heritage.py
@@ -12,6 +12,7 @@ st.title("Adding!!")
 yr_range=list(range(1900,2100))
 tp=["Natural","Cultural","Mixed"]
 rg=list(set(st.session_state.heritage1["REGION"]))
+cout=list(set(st.session_state.heritage1["COUNTRY"]))
 tab1, tab2=st.tabs(["Add Heritage", "Delete Heritage"])
 def bol_to_num(bol):
     if bol==True:
@@ -23,7 +24,7 @@ st.session_state
 with tab1:
     with st.form("my_form"):
         name = st.text_input("Name")
-        country= st.text_input("Country")
+        country= st.selectbox("Country",cout)
         region=st.selectbox("Region",rg)
         year=st.selectbox("Inscribed Year",yr_range)
         description=st.text_area("Description","NA")
@@ -46,15 +47,16 @@ with tab1:
         num=bol_to_num(tb)
         df1={'NAME':[name],
             'COUNTRY':[country],
-            'INSCRIBDATE':[year],
+            'DATEINSCRI':[year],
             'DESCRIPTIO':[description],
             'REGION':[region],
             'LONGITUDE':[float(x_cord)],
             'LATITUDE':[float(y_cord)],
-            "TRANSBOUND ":[num]}
+            "TRANSBOUND":[num]}
         gdf = gpd.GeoDataFrame(df1, geometry=gpd.points_from_xy(df1['LONGITUDE'], df1['LATITUDE']))
         st.dataframe(data=gdf,use_container_width=True)
-        st.session_state.heritage1=pd.concat([gdf,st.session_state.heritage1], axis=0, join='outer')
+        if st.button("Confirm"):
+            st.session_state.heritage1=pd.concat([gdf,st.session_state.heritage1], axis=0, join='outer')
             
 with tab2:
     st.dataframe(data=st.session_state.heritage1, use_container_width=True)
