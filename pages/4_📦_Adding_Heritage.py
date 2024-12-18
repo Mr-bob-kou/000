4cee8c15bc57e30cac5b5cfdbe47a7b87353046e
Mr-bob-kou <bob323565@gmail.com>
2024-12-18 16:48:52 +0800

    Update 4_📦_Adding_Heritage.py

--- pages/4_📦_Adding_Heritage.py
+++ pages/4_📦_Adding_Heritage.py
@@ -77,7 +77,7 @@ with tab3:
     id= st.session_state.heritage1[st.session_state.heritage1['NAME'] == row_name].index
     button3=st.button("Delete the Row")
     if button3:
-        st.session_state.heritage1.drop(id)
+        st.session_state.heritage1=st.session_state.heritage1.drop(id)
         st.write("Complete")
     
 
