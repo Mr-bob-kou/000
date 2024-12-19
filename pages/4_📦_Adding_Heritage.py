c261f710fe7e35b3efbbfe11b3ae8f9bc4306cf6
Mr-bob-kou <bob323565@gmail.com>
2024-12-19 14:10:53 +0800

    Update 4_📦_Adding_Heritage.py

--- pages/4_📦_Adding_Heritage.py
+++ pages/4_📦_Adding_Heritage.py
@@ -80,7 +80,7 @@ with tab3:
     button3=st.button("Delete the Row")
     if button3:
         st.session_state.heritage1=st.session_state.heritage1.drop(id)
-        st.write("Complete")
+        st.write("Wait 3 minetue")
         time.sleep(3)
         st.rerun()
     
