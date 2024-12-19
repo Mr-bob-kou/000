09c708dd38161f395ca90a035cef1a1e66dda82b
Mr-bob-kou <bob323565@gmail.com>
2024-12-19 22:39:31 +0800

    Update 3_🌍_Heritage_Near_Me.py

--- pages/3_🌍_Heritage_Near_Me.py
+++ pages/3_🌍_Heritage_Near_Me.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+from geopy import distance
 
 
 datum=st.session_state.heritage1
 lon="LONGITUDE"
 lat="LATITUDE"
-
+def calculate_distance(row):
+    city_coordinates = (row['LATITUDE'], row['LONGITUDE'])
+    return distance.geodesic(city_coordinates, home_city_coordinates).km
 
 st.title("Heritage Near Me")
 col1,col2=st.columns([4,1])
@@ -27,7 +30,13 @@ with col4:
 
 
 with col3:
-    #if st.session_state.search==True:
+    if st.session_state.search==True:
+        home_city_coordinates =[x_cord,y_cord]
+        result= datum.apply(calculate_distance, axis=1)
+        datum['distance_from_home'] = result
+        miun=datum[datum['distance_from_home']==datum['distance_from_home'].min()]
+        name=miun["NAME"]
+        st.write("The Nearest Heritage is:",name )
     m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
     m.add_points_from_xy(datum,x=lon,y=lat,popup=['NAME','COUNTRY','REGION','DATEINSCRI'])
     m.add_basemap(basemap)
