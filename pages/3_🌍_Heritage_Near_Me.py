279f87ebf51011adb8780f9188b4f08f5306998b
Mr-bob-kou <bob323565@gmail.com>
2024-12-19 21:46:35 +0800

    Update 3_🌍_Heritage_Near_Me.py

--- pages/3_🌍_Heritage_Near_Me.py
+++ pages/3_🌍_Heritage_Near_Me.py
@@ -11,17 +11,25 @@ lat="LATITUDE"
 
 
 st.title("Heritage Near Me")
-
-col1, col2 = st.columns([4, 1])
+col1,col2=st.columns([4,1])
+col3, col4 = st.columns([4, 1])
 options = list(leafmap.basemaps.keys())
 index = options.index("OpenTopoMap")
-
+with col1:
+    col5,col6= st.columns([1,1])
+    with col5:
+        x_cord=st.text_input("Longitude")
+    with col6:
+        y_cord=st.text_input("Latitude")
 with col2:
+    button=st.button("Searching")
+with col4:
 
     basemap = st.selectbox("Select a basemap:", options, index)
+    chx=st.toggle("Activate function A ?(Premium Member Only)")
 
 
-with col1:
+with col3:
 
     m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
     m.add_points_from_xy(datum,x=lon,y=lat,popup=['NAME','COUNTRY','REGION','DATEINSCRI'])
