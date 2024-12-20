4ac66eb23e21178e15bf8795296d93425dd50aff
Mr-bob-kou <bob323565@gmail.com>
2024-12-20 17:38:14 +0800

    Update 3_🌍_Heritage_Near_Me.py

--- pages/3_🌍_Heritage_Near_Me.py
+++ pages/3_🌍_Heritage_Near_Me.py
@@ -12,7 +12,10 @@ lat="LATITUDE"
 def calculate_distance(row):
     city_coordinates = (row['LATITUDE'], row['LONGITUDE'])
     return distance.geodesic(city_coordinates, home_city_coordinates).km
-
+if 'cordx' not in st.session_state:
+    st.session_state.cordx=""
+if 'cordy' not in st.session_state:
+    st.session_state.cordy=""
 st.title("Heritage Near Me")
 col1,col2=st.columns([4,1])
 col3, col4 = st.columns([4, 1])
@@ -22,15 +25,15 @@ st.session_state
 with col1:
     col5,col6,col7= st.columns([2,2,1],vertical_alignment="bottom")
     with col5:
-        x_cord=st.text_input("Longitude",key='cordx')
+        x_cord=st.text_input("Longitude",value=st.session_state.cordx)
     with col6:
-        y_cord=st.text_input("Latitude",key='cordy')
+        y_cord=st.text_input("Latitude",value=st.session_state.cordy)
     with col7:
         button=st.button("Searching",key='search')
 with col4:
 
     basemap = st.selectbox("Select a basemap:", options, index)
-    chx=st.toggle("Activate function A ?(Premium Member Only)",)
+    chx=st.toggle("Activate function A ?(Premium Member Only)")
 
 
 with col3:
@@ -41,9 +44,10 @@ with col3:
         output=st_folium(m1, width=700, height=500)
         fol_lat=output["last_clicked"]['lat']
         fol_long=output["last_clicked"]['lng']
-        st.write(127)
-        #st.session_state.cordx=float(fol_long)
-        #st.session_state.cordy=float(fol_lat)
+        if x_cord != st.session_state.cordx:
+            st.session_state.cordx=fol_long
+        if y_cord != st.session_state.cordx:
+            st.session_state.cordy=fol_lat
     else:
         m = leafmap.Map(locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
         m.add_points_from_xy(datum,x=lon,y=lat,popup=['NAME','COUNTRY','REGION','DATEINSCRI'])
