8060f5237c88420dc838f10cc679eeb5c4f558b0
Mr-bob-kou <bob323565@gmail.com>
2024-12-21 21:47:54 +0800

    Update 4_📦_Adding_Heritage.py

--- pages/4_📦_Adding_Heritage.py
+++ pages/4_📦_Adding_Heritage.py
@@ -98,8 +98,8 @@ with tab1:
         gdf = gpd.GeoDataFrame(df1, geometry=gpd.points_from_xy(df1['LONGITUDE'], df1['LATITUDE']))
         st.dataframe(data=gdf,use_container_width=True)
         st.session_state.heritage1=pd.concat([st.session_state.heritage1,gdf], axis=0, join='outer',ignore_index=True)
-        #time.sleep(5)
-        #st.rerun()
+        time.sleep(5)
+        st.rerun()
             
 with tab2:
     edit_df=st.data_editor(st.session_state.heritage1, use_container_width=True,num_rows="dynamic")
