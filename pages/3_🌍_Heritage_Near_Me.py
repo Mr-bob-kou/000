65a21c89e4ab9debc15d9f357082ea42c96bcf2b
Mr-bob-kou <bob323565@gmail.com>
2024-12-21 13:52:19 +0800

    Update 3_🌍_Heritage_Near_Me.py

--- pages/3_🌍_Heritage_Near_Me.py
+++ pages/3_🌍_Heritage_Near_Me.py
@@ -72,7 +72,7 @@ with col3:
             name="Heritage",
             marker=folium.Marker(icon=folium.Icon(icon='info-sign')),
             tooltip=tooltip2).add_to(marker_cluster)
-        output=st_folium(m1, width=700, height=500)
+        output=st_folium(m1, use_container_width=True)
         if output["last_clicked"] is None:
             st.write("Click the map and get latitude and longitude!!")
         else:
