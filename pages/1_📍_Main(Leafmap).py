c47b4041ccfda288f7753f5e179640a4bba36685
Mr-bob-kou <bob323565@gmail.com>
2024-12-21 17:36:45 +0800

    Update 1_📍_Main(Leafmap).py

--- pages/1_📍_Main(Leafmap).py
+++ pages/1_📍_Main(Leafmap).py
@@ -203,7 +203,7 @@ with col1:
         pop=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"]
         m=leafmap.Map(center=[40, -100], zoom=4)
         m.add_geojson(regions, layer_name="Countries",zoom_to_layer=False)
-        m.add_points_from_xy(heritage,x="LONGITUDE",y="LATITUDE", popup=pop,color_column='CATSHORT',icon_colors=['white','green','red'])
+        m.add_points_from_xy(heritage,x="LONGITUDE",y="LATITUDE", popup=pop,color_column='CATSHORT',marker_colors=['white','green','red'],icon_colors=['white','green','red'])
         m.add_basemap(basemap)
         m.to_streamlit(height=700)
         
