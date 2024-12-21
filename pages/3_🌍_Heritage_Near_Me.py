71804de3372f016beb453e24a162ceb63a7a0519
Mr-bob-kou <bob323565@gmail.com>
2024-12-21 14:04:24 +0800

    Update 3_🌍_Heritage_Near_Me.py

--- pages/3_🌍_Heritage_Near_Me.py
+++ pages/3_🌍_Heritage_Near_Me.py
@@ -92,7 +92,7 @@ with col3:
             miun=datum[datum['distance_from_home']==datum['distance_from_home'].min()]
             name=miun["NAME"].to_string(index=False)
             mini_dis=miun["distance_from_home"].to_string(index=False)
-            mult=st.pill(" ",muti_options,default='The Nearest')
+            mult=st.pills(" ",muti_options,default='The Nearest')
             if mult=="The Nearest":
                 st.write("The Nearest Heritage is:",name )
                 st.write("The Minimum Distance is:",mini_dis,"km" )
