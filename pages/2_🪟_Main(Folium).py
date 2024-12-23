59f844a752aca2ec117d470e6c5fc0c9b9a484ab
Mr-bob-kou <bob323565@gmail.com>
2024-12-23 17:12:45 +0800

    Update 2_🪟_Main(Folium).py

--- pages/2_🪟_Main(Folium).py
+++ pages/2_🪟_Main(Folium).py
@@ -22,6 +22,19 @@ if "disable_type" not in st.session_state:
     st.session_state.disable_inscdate=True
     st.session_state.disable_chbox=True
 
+def to_df(datum,val):
+    couda=datum.groupby(val).size()
+    couda.to_frame()
+    return couda.reset_index()
+
+def type(name,color,type_name,color_code,pop):
+    typee=heritage[heritage["CATSHORT"]==name]
+    m.add_points_from_xy(typee,x="LONGITUDE",y="LATITUDE", popup=pop,color_column='CATSHORT',marker_colors=[color],icon_colors=[color],add_legend=False)
+    legend_dict={type_name:color_code}
+    m.add_legend(title="Classification", legend_dict=legend_dict, draggable=False)
+
+
+
 st.title("Analysis")
 option=["Region","Catagory","Inscription Date"]
 #st.session_state
@@ -63,7 +76,40 @@ with col1:
             if"Inscription Date" in st.session_state.modes:
                 st.write("E")
             else:
-                st.write("F")
+                pop=["NAME","DATEINSCRI","COUNTRY","DESCRIPTIO","AREAHA","DANGER","LONGITUDE","LATITUDE"]
+                m=leafmap.Map(center=[40, -100], zoom=4)
+                m.add_geojson(regions, layer_name="Countries",zoom_to_layer=False)
+                ct_group=to_df(heritage,"CATSHORT")
+                ct_group.rename(columns={0:'count'},inplace=True)
+                df_group=pd.DataFrame({"Types":["Cultural","Mix","Natural"]})
+                ct_group=pd.concat([ct_group,df_group],axis=1)
+                st.write(ct_group)
+                if types=="See All":
+                    m.add_points_from_xy(heritage,x="LONGITUDE",y="LATITUDE", popup=pop,color_column='CATSHORT',marker_colors=['orange','green','red'],icon_colors=['white','green','red'],add_legend=False)
+                    legend_dict={"Cultural":"#FF8000",
+                                 "Natural":"#008000",
+                                 "Mixed":"#ff0000"}
+                    m.add_legend(title="Classification", legend_dict=legend_dict, draggable=False)
+                elif types=="Natural":
+                    type("N",'green',"Natural","#008000",pop)
+                elif types=="Cultural":
+                    type("C","orange","Cultural","#FF8000",pop)
+                elif types=="Mixed":
+                    type("C/N","red","Mixed","#ff0000",pop)
+                m.add_basemap(basemap)
+                m.to_streamlit(height=700)
+                if types=="See All":
+                    charts_cat = alt.Chart(ct_group).mark_bar(size=10).encode(x=alt.X("Types",type='nominal'),y=alt.Y("count",type="quantitative"))
+                    pie=alt.Chart(ct_group).mark_arc().encode(theta="count",color="Types")
+                else:
+                    cond=alt.condition(alt.datum.Types==types,alt.value('red'),alt.value('steelblue'))
+                    charts_cat = alt.Chart(ct_group).mark_bar(size=10).encode(x=alt.X("Types",type='nominal'),y=alt.Y("count",type="quantitative"),color=cond)
+                pie=alt.Chart(ct_group).mark_arc().encode(theta="count",color="Types")
+                col5,col6,col7=st.columns([1,1,3])
+                with col5:
+                    st.altair_chart(charts_cat)
+                with col6:
+                    st.altair_chart(pie)
         elif "Inscription Date" in st.session_state.modes:
             st.write("G")
         else:
