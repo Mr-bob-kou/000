783f79efff60a36bacd9bf63441d9a1d5341f778
Mr-bob-kou <bob323565@gmail.com>
2024-12-24 20:38:04 +0800

    Update 3_🌍_Heritage_Near_Me.py

--- pages/3_🌍_Heritage_Near_Me.py
+++ pages/3_🌍_Heritage_Near_Me.py
@@ -114,6 +114,7 @@ with col3:
             zoom1=15
             m = leafmap.Map(center=center1,zoom=zoom1,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
             m.add_points_from_xy(datum,x=lon,y=lat,popup=['NAME','COUNTRY','REGION','DATEINSCRI'])
+            m.add_marker(center1,color='red')
             m.add_basemap(basemap)
             m.to_streamlit(height=700)
             st.write("The Nearest Heritage is:",name )
