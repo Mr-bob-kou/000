58d3de82bc31315fd54784a3f4275fd66d5cd53f
Mr-bob-kou <bob323565@gmail.com>
2024-12-24 11:51:03 +0800

    Update 2_🪟_Main(Folium).py

--- pages/2_🪟_Main(Folium).py
+++ pages/2_🪟_Main(Folium).py
@@ -193,13 +193,13 @@ with col1:
                 with col7:
                     cuml(years, 'count')
                     cond=alt.condition(alt.datum.DATEINSCRI==Inscdate,alt.value('red'),alt.value('steelblue'))
-                    line_charts = alt.Chart(years).mark_line().encode(x=alt.X("DATEINSCRI",type='temporal'),y=alt.Y(alt.repeat("layer"),type='quantitative',title='count'), color=alt.datum(alt.repeat("layer"))).repeat(layer=["C", "C/N","N"])
+                    #line_charts = alt.Chart(years).mark_line().encode(x=alt.X("DATEINSCRI",type='temporal'),y=alt.Y(alt.repeat("layer"),type='quantitative',title='count'), color=alt.datum(alt.repeat("layer"))).repeat(layer=["C", "C/N","N"])
                     point_charts=alt.Chart(years).mark_point(filled=True,opacity=1).encode(x=alt.X("DATEINSCRI", type='temporal'),y=alt.Y(alt.repeat("layer"),type='quantitative'),color=cond).repeat(layer=["C", "C/N","N"])
-                    charts1=line_charts+point_charts
+                    #charts1=line_charts+point_charts
                     charts2 = alt.Chart(years).mark_bar(size=10).encode(x=alt.X("DATEINSCRI",type='temporal'),y=alt.Y("count",type="quantitative"),color=cond)
                     charts3= alt.Chart(years).mark_line().encode(x=alt.X("DATEINSCRI",type='temporal'),y=alt.Y("agrr",type="quantitative"))
                     if Chart_mode=='Line Chart':
-                        st.altair_chart(charts1,use_container_width=True)
+                        st.altair_chart(point_charts,use_container_width=True)
                     if Chart_mode=='Bar Chart':
                         st.altair_chart(charts2,use_container_width=True)
                     if Chart_mode=='Cumulative Line Chart':
